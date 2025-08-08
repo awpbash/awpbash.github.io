b727f8c5dca19d2e501e2c4c915c0c4893f8fffd
awpbash <ngjunwei06082001@gmail.com>
2025-08-08 17:12:54 +0800

    pls

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://awpbash.github.io',
+  integrations: [mdx(), sitemap(), tailwind()],
+  base: '/',
 });
